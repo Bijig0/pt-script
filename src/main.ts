dcf7d843669f75ca663e49797f2f4094062b5314
Brady <bradysuryasie@gmail.com>
2024-01-19 18:44:04 +1100

    reading ok

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,39 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import { parse } from 'csv-parse';
+import * as fs from "fs";
+import * as path from "path";
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+type WorldCity = {
+  name: string;
+  country: string;
+  subCountry: string;
+  geoNameId: number;
+};
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
+const __dirname = new URL('.', import.meta.url).pathname;
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+
+(async () => {
+  const csvFilePath = path.resolve(__dirname, 'output.csv');
+
+  const headers = ['tanggal', 'companyName', 'masuk', 'keluar'];
+
+  const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
+
+  const records: WorldCity[] = []
+
+  const parser = parse(fileContent, {
+    delimiter: ',',
+    columns: headers,
+    skipEmptyLines: true,
+  });
+
+
+  for await (const record of parser) {
+    // Work with each record
+    records.push(record);
+  }
+
+  console.log(records)
+  return records;
+})();
+
